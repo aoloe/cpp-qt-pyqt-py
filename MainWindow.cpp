8f1c7b9bcf14f81e174e783b437d920fb4742441
a.l.e <ale@ideale.ch>
2016-01-18 22:00:14 +0100

    can now run a python script

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -3,6 +3,12 @@
 #include <QMenuBar>
 #include <QDebug>
 
+#include <stdio.h>
+
+#undef slots // http://stackoverflow.com/questions/27045323/embedding-python-3-4-into-c-qt-application
+#include <Python.h>
+#define slots
+
 MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags)
     : QMainWindow(parent, flags)
 {
@@ -10,6 +16,18 @@ MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags)
     setWindowTitle("cpp-qt-pyqt-py");
 
     setupMenuBar();
+
+    // PyObject* PyRun_File(FILE *fp, const char *filename, int start, PyObject *globals, PyObject *locals)
+    // FILE* file = fopen("test.py","r");
+    // PyRun_SimpleFile(file, "test.py");
+
+    PyObject *obj = Py_BuildValue("s", "test.py");
+    FILE *file = _Py_fopen_obj(obj, "r+");
+    if(file != NULL) {
+        PyRun_SimpleFile(file, "test.py");
+    } else {
+        qDebug() << "file test.py not found (did you copy it in the build directory";
+    }
 }
 
 void MainWindow::setupMenuBar()
@@ -17,7 +35,7 @@ void MainWindow::setupMenuBar()
         testMenu = menuBar()->addMenu(tr("&Test"));
 
         QAction *action = testMenu->addAction(tr("&Go"));
-        connect(action, SIGNAL(triggered()), this, SLOT(go()));
+        connect(action, SIGNAL(triggered()), this, SLOT(letsGo()));
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include<QApplication>
 
-// #include <Python.h>
-// #undef B0 // http://stackoverflow.com/questions/15078060/embedding-python-in-qt-5
+#undef slots // http://stackoverflow.com/questions/27045323/embedding-python-3-4-into-c-qt-application
+#include <Python.h>
+#define slots
+#undef B0 // http://stackoverflow.com/questions/15078060/embedding-python-in-qt-5
 
 #include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
-    // Py_Initialize();
+    Py_Initialize();
 
     QApplication app(argc, argv);
     MainWindow window;
     window.resize(800, 600);
     window.show();
-    return app.exec();
+    int result = app.exec();
+    Py_Finalize();
+    return result;
 }
